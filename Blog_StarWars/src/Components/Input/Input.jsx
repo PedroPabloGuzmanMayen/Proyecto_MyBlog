4eaf576920dbca4cd9175d7ac4276355cda03640
ppguz <ppguzmanmayen@gmail.com>
2024-04-26 00:15:47 -0600

    Chore: added type parameter to input component

--- Blog_StarWars/src/Components/Input/Input.jsx
+++ Blog_StarWars/src/Components/Input/Input.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Proptypes from 'prop-types'
 import './Input.css'
 
-const Input = ({ onChange, value, text, placeholder }) => {
+const Input = ({ onChange, value, text, placeholder, type }) => {
     return (
         <label className = 'Input-container'>
             <span className = 'text'>{text}</span>
-            <input onChange={onChange} className = 'Input' value={value} placeholder = {placeholder} />
+            <input onChange={onChange} className = 'Input' value={value} placeholder = {placeholder} type = {type} />
         </label>
     )
 }
@@ -15,7 +15,8 @@ Input.propTypes = {
     onChange: Proptypes.func,
     value: Proptypes.string,
     text: Proptypes.string,
-    placeholder: Proptypes.string
+    placeholder: Proptypes.string,
+    type: Proptypes.string
 }
 
 export default Input
