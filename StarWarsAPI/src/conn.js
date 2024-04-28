24922d2ec09dbe8e6fa89e2a038d9ae0b49e126b
ppguz <ppguzmanmayen@gmail.com>
2024-04-27 21:58:59 -0600

    Feat: created script for handle connection with db

---
+++ StarWarsAPI/src/conn.js
@@ -0,0 +1,13 @@
+import pg from 'pg';
+
+const { Client } = pg;
+const connectionData = {
+  user: 'pp',
+  host: 'localhost',
+  database: 'blog_starwars',
+  password: 'hola',
+  port: 5432,
+};
+const client = new Client(connectionData);
+client.connect();
+export default client;
