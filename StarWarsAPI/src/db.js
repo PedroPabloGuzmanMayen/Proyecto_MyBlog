fb1841efbc4bb2cd42cf675cd989b46c29c3ec9c
ppguz <ppguzmanmayen@gmail.com>
2024-04-28 15:28:47 -0600

    Fix: fixed return values for db querys

--- StarWarsAPI/src/db.js
+++ StarWarsAPI/src/db.js
@@ -4,7 +4,7 @@ import conn from './conn.js'
 export async function getPosts() {
   try {
     const rows = await conn.query('SELECT * FROM posts')
-    return rows
+    return rows.rows
   } catch (error) {
     console.error('Error obtienedo posts:', error)
     throw error
@@ -15,33 +15,33 @@ export async function modifyPostByID(id, title, content, image) {
   // Modificar imagen y contenido pero no el título
   try {
     if (title === '' && content !== '' && image !== '') {
-      const [res] = await conn.query('UPDATE posts SET content = ?, banner = ? WHERE id = ?', [content, image, id])
-      return res
+      const res = await conn.query('UPDATE posts SET content = ?, banner = ? WHERE id = ?', [content, image, id])
+      return res.rows
     }
     // Modificar el título y el contenido pero no la imagen
     if (content !== '' && title !== '' && image === '') {
-      const [res] = await conn.query('UPDATE posts SET content = ?, title = ? WHERE id = ?', [title, content, id])
-      return res
+      const res = await conn.query('UPDATE posts SET content = ?, title = ? WHERE id = ?', [title, content, id])
+      return res.rows
     }
     // Modificar el título y la imagen pero no el contenido
     if (content === '' && image !== '' && title !== '') {
-      const [res] = await conn.query('UPDATE posts SET title = ?, banner = ? WHERE id = ?', [title, image, id])
-      return res
+      const res = await conn.query('UPDATE posts SET title = ?, banner = ? WHERE id = ?', [title, image, id])
+      return res.rows
     }
     // Modificar solo el contenido
     if (content !== '' && image === '' && title === '') {
-      const [res] = await conn.query('UPDATE posts SET content = ? WHERE id = ?', [content, id])
-      return res
+      const res = await conn.query('UPDATE posts SET content = ? WHERE id = ?', [content, id])
+      return res.rows
     }
     // Modificar solo el título
     if (content === '' && image === '' && title !== '') {
-      const [res] = await conn.query('UPDATE posts SET title = ? WHERE id = ?', [title, id])
-      return res
+      const res = await conn.query('UPDATE posts SET title = ? WHERE id = ?', [title, id])
+      return res.rows
     }
     // Modificar solo la imagen
     if (content === '' && image !== '' && title === '') {
-      const [res] = await conn.query('UPDATE posts SET banner = ? WHERE id = ?', [image, id])
-      return res
+      const res = await conn.query('UPDATE posts SET banner = ? WHERE id = ?', [image, id])
+      return res.rows
     }
     // Modificar todo el post
 
@@ -55,8 +55,8 @@ export async function modifyPostByID(id, title, content, image) {
 // Con esta función se crea un nuevo post, se deben verificar los valores que se desean modificar
 export async function newPost(title, content, image, author) {
   try {
-    const [rows] = await conn.query('INSERT INTO posts (title, content, banner, author) VALUES (?, ?, ?, ?)', [title, content, image, author])
-    return rows
+    const rows = await conn.query('INSERT INTO posts (title, content, banner, author) VALUES (?, ?, ?, ?)', [title, content, image, author])
+    return rows.rows
   } catch (error) {
     console.error('Error creando post:', error)
     throw error
@@ -66,8 +66,8 @@ export async function newPost(title, content, image, author) {
 // Con esta función se obtiene un post dado su ID
 export async function getPostbyID(id) {
   try {
-    const [result] = await conn.query('SELECT * FROM posts WHERE id = ?', [id])
-    return result
+    const result = await conn.query('SELECT * FROM posts WHERE id = ?', [id])
+    return result.rows
   } catch (error) {
     console.error('Error obteniendo post:', error)
     throw error
@@ -77,7 +77,7 @@ export async function getPostbyID(id) {
 export async function deletePost(id) {
   try {
     const [res] = await conn.query('DELETE FROM posts WHERE id = ?', [id])
-    return res
+    return res.rows
   } catch (error) {
     console.error('Error eliminando post:', error)
     throw error
