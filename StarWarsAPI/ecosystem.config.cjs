53868ec361fa52e05de951b071cd0b7cc9a99180
ppguz <ppguzmanmayen@gmail.com>
2024-04-27 22:50:13 -0600

    Fix: npm install not working

--- StarWarsAPI/ecosystem.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-        name: 'blog_starwars',
-        script: 'npm',
-        args: 'start',
-        log_file: 'blog_starwars.txt',
-        merge_logs: true
-    }]
-}
