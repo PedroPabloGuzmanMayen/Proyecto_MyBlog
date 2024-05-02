591e664c0e5ccecbc79cdf4ad1e4c82b1c6742c3
ppguz <ppguzmanmayen@gmail.com>
2024-05-01 19:25:46 -0600

    Fix: cors middleware

--- StarWarsAPI/src/main.js
+++ StarWarsAPI/src/main.js
@@ -23,12 +23,6 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  next()
-})
 
 app.get('/', (req, res) => {
   res.send('Hello from a galaxy far far away!')
